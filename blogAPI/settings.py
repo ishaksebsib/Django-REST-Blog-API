0cafcab549eb94d471a4f5fb0f22f4b5f29c0d52
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-06-29 11:52:59 +0300

    update

--- blogAPI/settings.py
+++ blogAPI/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local
+    "accounts.apps.AccountsConfig",
 ]
+
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
